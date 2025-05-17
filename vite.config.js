15a35eecb456338e97df0553bb88e891373146f2
Huỳnh Văn Chí Khan <165972242+huynhvanchikhan-2180604870@users.noreply.github.com>
2025-05-18 03:17:19 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  preview: {
+    allowedHosts: ['proxy-check-app-4cd25acb8348.herokuapp.com'] 
+  }
 });
